cdffb2a832d99b7c669829af529db22d06889b23
edgarrada <116873249+edgarrada@users.noreply.github.com>
2022-10-30 10:35:45 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+// img2
+alert("Double tap my picture to open hehe")
+alert("Sorry sa mga maling margin")
+alert("Hindi kopa gamay ang media query sorry  na uwu next time gagalingan kopa")
+alert("Play mo yung video")
+
+function img6(){
+    document.querySelector('#img6')
+    .addEventListener('click', () => {
+        window.location.href = 'tabibee.html'
+    })
+}
+
+function manages(){
+    document.getElementById("manages").classList.toggle("active")
+}
---
+++ tabibee.js
@@ -0,0 +1,13 @@
+function img1(){
+    document.querySelector('#img1')
+    .addEventListener('click', () => {
+        window.location.href = 'tabibee.html'
+    })
+}
+
+function account(){
+    document.querySelector('#account')
+    .addEventListener('click', () => {
+        window.location.href = 'https://www.facebook.com/tabetlog'
+    }) 
+}
